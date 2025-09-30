aff93cee1cfcde30220061a4bea071d24703f365
ST10303286 <130739564+ST10303286@users.noreply.github.com>
2025-09-30 12:35:11 +0200

    Implemented the Login backend to work with: Account Number, Username, and Password

--- backend/service/userService.js
+++ backend/service/userService.js
@@ -17,28 +17,45 @@ const ACCOUNTNUMBER_REGEX = /^[0-9]+$/
 const USERNAME_REGEX = /^[a-zA-Z][a-zA-Z0-9_]{2,15}$/
 
 async function loginUser(data) {
-    const { email, password } = data;
+    const { accountNumber, password, username } = data;
 
-    if (!email || !password) {
-        throw new Error("Email and password are required");
+    if ( !accountNumber || !password|| !username) {
+        throw new Error("All fields are required");
+    }
+    if(!PASSWORD_REGEX.test(password)){
+        throw new Error("Invalid password format. Please include a number, uppercase and a special character and ensure its 10 characters in length")
+    }
+
+    if(!ACCOUNTNUMBER_REGEX.test(accountNumber)){
+        throw new Error("Invalid account number")
+    }
+
+    if(!USERNAME_REGEX.test(username)){
+        throw new Error("Invalid username. Username must start with a letter and be a minimum of 3 characters")
     }
 
     try {
-        // Find user by email
-        const user = await userCollection.findOne({ email });
+        // Find user by Username
+        const user = await userCollection.findOne({username });
         if (!user) {
-            throw new Error("Invalid email or password");
+            throw new Error("Invalid username, account number or password");
         }
 
         // Compare password
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            throw new Error("Invalid email or password");
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            throw new Error("Invalid username, account number or password");
+        }
+
+        // Compare account number
+        const isAccountNumberMatch = await bcrypt.compare(accountNumber, user.accountNumber);
+        if (!isAccountNumberMatch) {
+            throw new Error("Invalid username, account number or password");
         }
 
         // Generate JWT token
         const token = jwt.sign(
-        { email },
+        { username },
         process.env.JWT_SECRET,
         { expiresIn: "1h" }
         );
@@ -47,7 +64,7 @@ async function loginUser(data) {
         return {
             message: "Login successful",
             userId: user._id,
-            email: user.email,
+            username: user.username,
             token: token
         };
     } catch (error) {
